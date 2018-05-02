3dc3a32f9053573812b41f7e5799c57d4cf8d729
D7703_23 <d7703_23@D7703-23ui-iMac.local>
2018-05-02 12:59:21 +0900

    Initial Commit

---
+++ TableView Test/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  TableView Test
+//
+//  Created by D7702_10 on 2018. 5. 02..
+//  Copyright © 2018년 jik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    var data = ["1","2","3"]
+    var subData = ["ONE", "TWO", "THREE"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier = "RE"
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        
+        cell.textLabel?.text = data[indexPath.row]
+        cell.detailTextLabel?.text = "section= \(indexPath.section) row=\(indexPath.row)"
+        
+        let myImg = UIImage(named:"\(indexPath.row+1).jpg")
+
+        cell.imageView?.image = myImg
+        
+        return cell
+    }
+    
+
+    @IBOutlet weak var myTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        myTableView.dataSource = self
+        myTableView.delegate = self
+        
+    }
+
+
+
+}
+
